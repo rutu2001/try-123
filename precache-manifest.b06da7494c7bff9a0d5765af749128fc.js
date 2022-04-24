15084fca8b3482e9c14d535b6bfb8ee7d78bb8cc
Rutuja Mali <59461652+rutu2001@users.noreply.github.com>
2022-04-24 12:57:20 +0530

    Updates

---
+++ precache-manifest.b06da7494c7bff9a0d5765af749128fc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5c1d3a0c10fa2854fd7",
+    "url": "/static/css/main.a158ea15.chunk.css"
+  },
+  {
+    "revision": "f5c1d3a0c10fa2854fd7",
+    "url": "/static/js/main.f5c1d3a0.chunk.js"
+  },
+  {
+    "revision": "fb1bb6bc4adbd6213085",
+    "url": "/static/css/1.5e3737d5.chunk.css"
+  },
+  {
+    "revision": "fb1bb6bc4adbd6213085",
+    "url": "/static/js/1.fb1bb6bc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6f077f862e757d833056bd2369a7d4ba",
+    "url": "/static/media/bg3.6f077f86.jpg"
+  },
+  {
+    "revision": "86db93eef72a89edbcedd3b685d7a035",
+    "url": "/static/media/temp.86db93ee.png"
+  },
+  {
+    "revision": "04dba6c240863e343c9698b45a9a7f50",
+    "url": "/static/media/bg2.04dba6c2.jpg"
+  },
+  {
+    "revision": "8e328d2ce1cd47f0a6d58a39883ebcab",
+    "url": "/static/media/end.8e328d2c.jpg"
+  },
+  {
+    "revision": "b2b0c87d54ffdb9a4a56f47f5beb9e60",
+    "url": "/static/media/middle.b2b0c87d.jpg"
+  },
+  {
+    "revision": "155085107167a234ed78e87789bcaaa6",
+    "url": "/index.html"
+  }
+];
